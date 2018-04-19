24fc51dd1410bb3b874f578f88ab9df8adbfb31c
Ricardo Ozelo <ozelinhofilho@hotmail.com>
2018-04-19 01:01:31 -0300

    OOF

--- js/mem.js
+++ js/mem.js
@@ -1,6 +1,9 @@
-function populateTable(tableid) {
-	var gm_width = 4;
-	var gm_height = 4;
+var finalPairs = {};
+var everyButton = {};
+
+function populateTable(tableid,gamesize) {
+	var gm_width = gamesize;
+	var gm_height = gamesize;
 
 	var rows = "";
 
@@ -18,32 +21,90 @@ function populateTable(tableid) {
 }
 
 function pairButtons() {
-	var allButtons = document.getElementsByClassName("imgBTN");
+	var allButtonsHTML = document.getElementsByClassName("imgBTN");
+
+	var allButtons = HTMLtoArray(allButtonsHTML);
+	var cells = allButtons.length/2;
 	var buttonPairs = new Array();
 	//buttonPairs[0] = new Array();
 
-	console.log(buttonPairs);
-
-	for (var i = 0; i < allButtons.length/2; i++) {
+	for (var i = 0; i < cells; i++) {
 		buttonPairs[i] = new Array(2);
-		var ran = randomIntFromInterval(0,allButtons.length);
+		var ran = getRandomInt(0,allButtons.length-1);
 		buttonPairs[i][0] = allButtons[ran];
 		allButtons.splice(ran, 1);
 
-		ran = randomIntFromInterval(0,allButtons.length);
+		ran = getRandomInt(0,allButtons.length-1);
 		buttonPairs[i][1] = allButtons[ran];
 		allButtons.splice(ran, 1);
 	}
 
-	console.log(buttonPairs);
-	console.log(allButtons);
+	finalPairs = buttonPairs;
+
+	givePairsImages();
 }
 
-function randomIntFromInterval(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
+function givePairsImages() {
+	for (var i = 0; i < finalPairs.length; i++){
+		finalPairs[i][0].innerHTML = "<img class='image-button' src='images/"+i+".png'>"
+		finalPairs[i][0].imageid = i;
+		finalPairs[i][1].innerHTML = "<img class='image-button' src='images/"+i+".png'>"
+		finalPairs[i][1].imageid = i;
+	}
+
+	hideImages();
+}
+
+function hideImages() {
+	var allButtonsHTML = document.getElementsByClassName("imgBTN");
+
+	var allButtons = HTMLtoArray(allButtonsHTML);
+
+	for (var i = 0; i < allButtons.length; i++) {
+
+	}
+}
+
+function startGame(){
+
+	var radios = document.getElementsByName('difficulty');
+	var difficulty = 0;
+
+	for (var i = 0, length = radios.length; i < length; i++){
+		if (radios[i].checked){
+			difficulty = radios[i].value;
+		}
+	}
+
+	if(difficulty == 0){
+		document.getElementById('menu-warning').style.opacity = 1;
+	}else{
+		document.getElementById('dark-overlay').style.opacity = 0;
+		setTimeout(function () {
+			document.getElementById('dark-overlay').style.display = "none";
+			document.getElementById('game-table').style.opacity = 1;
+		}, 1000);
+		populateTable('gametable',difficulty);
+	}
+}
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+function HTMLtoArray(htmlcollection){
+	var returnArray = new Array;
+	for (var i = 0; i < htmlcollection.length; i++) {
+		returnArray.push(htmlcollection[i]);
+	}
+
+	return returnArray;
 }
 
 /*
 no comment
+
+document.getElementById(btnPairs[i][0]).innerHTML = "<img class='image-button' src='images/"+i+".png'>"
+		document.getElementById(btnPairs[i][1]).innerHTML = "<img class='image-button' src='images/"+i+".png'>"
 */
